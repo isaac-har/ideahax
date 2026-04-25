a0d1ed31e3e98196587b6f227049960a1b5223cc
Isaac Harrington <isaachar202@gmail.com>
2026-04-25 12:48:19 -0700

    Light sensor working

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,24 @@
 #include <bme68x_defs.h>
 #include <Adafruit_BME680.h>
 #include <Wire.h>
+#include <SparkFun_VEML6030_Ambient_Light_Sensor.h>
 
 Adafruit_BME680 bme;
-// put function declarations here:
+
+// AMBIENT LIGHT SENSOR CODE BELOW:
+#define AL_ADDR 0x48
+SparkFun_Ambient_Light light(AL_ADDR);
+
+// Possible values: .125, .25, 1, 2
+// Both .125 and .25 should be used in most cases except darker rooms.
+// A gain of 2 should only be used if the sensor will be covered by a dark
+// glass.
+float gain = .125;
+
+// Possible integration times in milliseconds: 800, 400, 200, 100, 50, 25
+// Higher times give higher resolutions and should be used in darker light.
+int lightTime = 100;
+long luxVal = 0;
 
 void setup()
 {
@@ -15,8 +30,6 @@ void setup()
 
   delay(2000);
   Wire.begin(12, 15); // SDA, SCL pins
-
-  Serial.println("Hello, World!");
 
   bool found = false;
   for (byte address = 1; address < 127; address++)
@@ -46,6 +59,23 @@ void setup()
   bme.setPressureOversampling(BME680_OS_4X);
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150); // 320*C for 150 ms
+
+  // Light sensor setup
+  if (light.begin())
+    Serial.println("Ready to sense some light!");
+  else
+    Serial.println("Could not communicate with the sensor!");
+
+  light.setGain(gain);
+  light.setIntegTime(lightTime);
+
+  Serial.println("Light Sensor Settings:");
+  Serial.print("Gain: ");
+  float gainVal = light.readGain();
+  Serial.print(gainVal, 3);
+  Serial.print(" Integration Time: ");
+  int timeVal = light.readIntegTime();
+  Serial.println(timeVal);
 }
 
 void loop()
@@ -63,6 +93,10 @@ void loop()
     Serial.println(bme.humidity);
     Serial.print("Pressure: ");
     Serial.println(bme.pressure / 100.0);
+    Serial.print("Ambient Light Reading: ");
+    Serial.print(light.readLight());
+    Serial.println(" Lux");
+    Serial.println();
   }
   delay(2000);
 }
